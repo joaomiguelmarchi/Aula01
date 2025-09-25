fbb789521bd2bc0d72b83182caea10196cc0c2c1
aluno <jmsmarchi@gmail.com>
2025-09-24 21:31:20 -0300

    imc iniciado

---
+++ app/src/main/java/com/example/myapplication/ActivityResultado.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class ActivityResultado extends AppCompatActivity {
+    public TextView txtResultado;
+    public TextView txtResultado2;
+    public ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+        Bundle bundle = getIntent().getExtras();
+        float valor = bundle.getFloat("msg");
+        txtResultado = findViewById(R.id.imcValorResultado);
+        txtResultado2 = findViewById(R.id.imcResultado);
+        img = findViewById(R.id.img);
+        DecimalFormat d = new DecimalFormat("#.#");
+        String s = String.valueOf(d.format(valor));
+        txtResultado.setText(s);
+        Resultado(valor);
+    }
+
+    public void Resultado(float valor){
+        if(valor<=18.5){
+            img.setImageResource(R.drawable.abaixopeso);
+            txtResultado2.setText("Abaixo do Peso");
+        } else {
+            if((valor>18.5)&&(valor<25)){
+                img.setImageResource(R.drawable.normal);
+                txtResultado2.setText("Peso Ideal");
+            } else{
+                if ((valor>=25)&&(valor<30)){
+                    img.setImageResource(R.drawable.sobrepeso);
+                    txtResultado2.setText("Sobrepeso");
+                } else{
+                    if((valor>=30)&&(valor<35)){
+                        img.setImageResource(R.drawable.obesidade1);
+                        txtResultado2.setText("Obesidade grau I");
+                    } else{
+                        if ((valor>=35)&&(valor<40)){
+                            img.setImageResource(R.drawable.obesidade2);
+                            txtResultado2.setText("Obesidade grau II");
+                        } else{
+                            if((valor>=40)){
+                                img.setImageResource(R.drawable.obesidade3);
+                                txtResultado2.setText("Obesidade morbida");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,43 +1,26 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
-    Button btn;
-    int i = 0;
-
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
-
-        btn = findViewById(R.id.button);
-        tv.setText(Integer.toString(i));
-
-        btn.setOnClickListener(v -> {
-            i++;
-           tv.setText(Integer.toString(i));
-        });
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
+    }
+    public void onCalcular(View v){
+        Intent intent = new Intent(getApplicationContext(),Activity2.class);
+        startActivity(intent);
+    }
+    public void onSobre(View v){
+        Intent intent = new Intent(getApplicationContext(),ActivitySobre.class);
+        startActivity(intent);
     }
 }
