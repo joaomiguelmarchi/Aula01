fb4e32076f31c0b7a3e5f0cda35a3add93db0d06
João Miguel <joao.marchi@lince.com.br>
2025-11-30 20:13:09 -0300

    Sorteador automatico

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,43 +1,77 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Button botao;
+    EditText numeroUm, numeroDois;
+    TextView resultado;
 
-    TextView tv;
-    Button btn;
-    int i = 0;
-
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
 
-        btn = findViewById(R.id.button);
-        tv.setText(Integer.toString(i));
+        botao = findViewById(R.id.botao);
+        numeroUm = findViewById(R.id.numeroUm);
+        numeroDois = findViewById(R.id.numeroDois);
+        resultado = findViewById(R.id.resultado);
 
-        btn.setOnClickListener(v -> {
-            i++;
-           tv.setText(Integer.toString(i));
+        botao.setOnClickListener(view -> {
+            final String txtNum1 = numeroUm.getText().toString();
+            final String txtNum2 = numeroDois.getText().toString();
+
+            if(txtNum1.isEmpty() || txtNum2.isEmpty()){
+                mostraAlerta(
+                        "Valor inválido",
+                        "Valor mínimo e/ou valor máximo devem ser informados"
+                );
+                return;
+            }
+
+            final int num1 = Integer.parseInt(txtNum1);
+            final int num2 = Integer.parseInt(txtNum2);
+
+            if (num1 > num2) {
+                mostraAlerta(
+                        "Valor inválido",
+                        "Valor mínimo não pode ser maior que o valor maximo"
+                );
+                return;
+            }
+            if(num2 == 0){
+                mostraAlerta(
+                        "Valor inválido",
+                        "Valor maximo não pode ser zero"
+                );
+                return;
+            }
+
+            resultado.setText(
+                    Integer.toString(gerarNumeroAleatorio(num1, num2))
+            );
         });
+    }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public int gerarNumeroAleatorio(int num1, int num2) {
+        final Random random = new Random();
+        return random.nextInt((num2 - num1) + 1) + num1;
+    }
 
+    public void mostraAlerta(String titulo, String mensagem) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder
+                .setTitle(titulo)
+                .setMessage(mensagem)
+                .setPositiveButton(R.string.btn_alert_ok, (dialogInterface, i) -> dialogInterface.dismiss())
+                .show();
     }
 }
